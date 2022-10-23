8effdddbbe559809136693bbd04db0166389d0a8
guastro23 <112786732+guastro23@users.noreply.github.com>
2022-10-22 23:25:18 -0500

    Add files via upload

---
+++ ejercicio 4_2/src/CarreraCiclística/Contrarrelojista.java
@@ -0,0 +1,32 @@
+
+package CarreraCiclística;
+
+public class Contrarrelojista extends Ciclista {
+
+private double velocidadMaxima;
+
+public Contrarrelojista(int identificador, String nombre, double 
+velocidadMaxima) {
+super(identificador, nombre);
+this.velocidadMaxima = velocidadMaxima;
+}
+
+protected double getVelocidadMaxima() {
+return velocidadMaxima;
+}
+
+protected void setVelocidadMaxima(double velocidadMaxima) {
+this.velocidadMaxima = velocidadMaxima;
+}
+
+protected void imprimir() {
+super.imprimir(); 
+System.out.println("Aceleración promedio = " + 
+velocidadMaxima);
+}
+
+protected String imprimirTipo() {
+return "Es un constrarrelojista";
+}
+}
+
